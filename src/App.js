7e1239cf123caa688a5f37f0bb024a79252cb745
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2025-04-18 12:50:42 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Products from './components/Products';
-import Cart from './components/Cart';
-import NoPage from './components/NoPage';
+import Home from './components/home';
 import HomeMain from './container/HomeMain';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
           <Route path="*" element={<Home />} />
-          <Route path="/GardenFresh" element={<HomeMain />} />,
-        </Route>
+          <Route path="/HomeMain" element={<HomeMain />} />
       </Routes>
     </BrowserRouter>
   );
