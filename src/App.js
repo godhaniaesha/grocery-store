2221314a1e8e7ef44f64c53a4486e5505473bf4f
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2025-04-19 09:14:39 +0530

    menuheader

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import HomeMain from './container/HomeMain';
+import Menuheader from './components/Menuheader';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="*" element={<Home />} />
+          <Route path="/Menuheader" element={<Menuheader />} />
           <Route path="/HomeMain" element={<HomeMain />} />
       </Routes>
     </BrowserRouter>
