7e2592033fa4db39188ddc74ca1645be4c397236
denishababariya <denishababariya899@gmail.com>
2025-04-26 09:08:25 +0530

    refactor(redux): restructure reducers and add category and subcategory slices
    Refactor the root reducer to use `combineReducers` from Redux Toolkit and introduce new slices for managing categories and subcategories. This improves maintainability and scalability of the state management system.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
 
-function store(props) {
-    return (
-        <div>
-            <h1>hgsdfj</h1>
-        </div>
-    );
-}
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+    devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
